631b1d39855fac71049132f3e8f2fe634ad21892
SBrick <samuelbrick3@gmail.com>
2022-03-30 12:38:51 -0500

    Refactored processing functions to be organized in a processor object

--- script.js
+++ script.js
@@ -49,6 +49,35 @@ async function retrieveWeatherInfo(latitude, longitude) {
     };
 };
 
+const processor = (() => {
+    function temperature(temperature) {
+        let celsius = Math.floor((temperature - 273.15));
+        let fahrenheit = Math.floor((((celsius * 9) / 5) + 32));
+        return [`${celsius}°C`, `${fahrenheit}°F`];
+    };
+    function weatherDescription(weatherDescription) {
+        let descriptionArray = weatherDescription.split("");
+        descriptionArray = descriptionArray.map((letter, index) => (index === 0 || descriptionArray[index - 1] === " ") ? letter.toUpperCase() : letter.toLowerCase());
+        descriptionString = descriptionArray.join("");
+        return descriptionString;
+    };
+    function windSpeed(windSpeed) {
+        let metersPerSec = windSpeed;
+        let milesPerHour = Math.round((windSpeed / 1609.34) * 3600);
+        return [`${metersPerSec} m/s`, `${milesPerHour} mph`];
+    };
+    function time(seconds) {
+        let time = new Date();
+        time.setTime(seconds * 1000);
+        let hour =  time.getHours() + 1;
+        let modifiedHour = (hour < 13) ? hour : hour - 12;
+        let minutes = time.getMinutes();
+        let timeIndicator = (hour < 13) ? "AM" : "PM";
+        return `${modifiedHour}:${minutes} ${timeIndicator}`
+    };
+    return {temperature, weatherDescription, windSpeed, time};
+})();
+
 async function generateWeatherInfo() {
     try {
         const coordinateInfo = await retrieveLocationCoordinates(cityInput.value, stateInput.value, countryInput.value);
@@ -57,43 +86,14 @@ async function generateWeatherInfo() {
         city = coordinateInfo[0].name;
         console.log(`${city}, ${state}, ${country}`);
         const weatherInfo = await retrieveWeatherInfo(coordinateInfo[0].lat, coordinateInfo[0].lon);
-        temperature = processTemperature(weatherInfo.main.temp);
-        weatherDescription = processWeatherDescription(weatherInfo.weather[0].description);
-        windSpeed = processWindSpeed(weatherInfo.wind.speed);
+        temperature = processor.temperature(weatherInfo.main.temp);
+        weatherDescription = processor.weatherDescription(weatherInfo.weather[0].description);
+        windSpeed = processor.windSpeed(weatherInfo.wind.speed);
         humidity = `${weatherInfo.main.humidity}%`;
-        sunrise = processTime(weatherInfo.sys.sunrise);
-        sunset = processTime(weatherInfo.sys.sunset);
+        sunrise = processor.time(weatherInfo.sys.sunrise);
+        sunset = processor.time(weatherInfo.sys.sunset);
         console.log(`Temp: ${temperature[0]} or ${temperature[1]}, Weather: ${weatherDescription}, Wind Speed: ${windSpeed[0]} or ${windSpeed[1]}, Humidity: ${humidity}, Sunrise: ${sunrise}, Sunset: ${sunset}`);
     } catch {
         err => console.error(err);
     };
-};
-
-function processTemperature(temperature) {
-    let celsius = Math.floor((temperature - 273.15));
-    let fahrenheit = Math.floor((((celsius * 9) / 5) + 32));
-    return [`${celsius}°C`, `${fahrenheit}°F`];
-};
-
-function processWeatherDescription(weatherDescription) {
-    let descriptionArray = weatherDescription.split("");
-    descriptionArray = descriptionArray.map((letter, index) => (index === 0 || descriptionArray[index - 1] === " ") ? letter.toUpperCase() : letter.toLowerCase());
-    descriptionString = descriptionArray.join("");
-    return descriptionString;
-};
-
-function processWindSpeed(windSpeed) {
-    let metersPerSec = windSpeed;
-    let milesPerHour = Math.round((windSpeed / 1609.34) * 3600);
-    return [`${metersPerSec} m/s`, `${milesPerHour} mph`];
-};
-
-function processTime(seconds) {
-    let time = new Date();
-    time.setTime(seconds * 1000);
-    let hour =  time.getHours() + 1;
-    let modifiedHour = (hour < 13) ? hour : hour - 12;
-    let minutes = time.getMinutes();
-    let timeIndicator = (hour < 13) ? "AM" : "PM";
-    return `${modifiedHour}:${minutes} ${timeIndicator}`
 };
